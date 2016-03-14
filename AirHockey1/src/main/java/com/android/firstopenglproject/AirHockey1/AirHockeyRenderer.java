9e4684179849f1367685518312550b4bae262de7
“divinechess” <johnallott27@hotmail.com>
2016-03-14 21:04:11 +0000

    First 3D board. updated Matrix data

--- AirHockey1/src/main/java/com/android/firstopenglproject/AirHockey1/AirHockeyRenderer.java
+++ AirHockey1/src/main/java/com/android/firstopenglproject/AirHockey1/AirHockeyRenderer.java
@@ -30,18 +30,21 @@ import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glDrawArrays;
 import static android.opengl.GLES20.glEnableVertexAttribArray;
 import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniformMatrix4fv;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
+import static android.opengl.Matrix.orthoM;
 
 
 
 
 public class AirHockeyRenderer implements Renderer {
-
+    private static final String U_MATRIX = "u_Matrix";
     private static final String A_POSITION = "a_Position";
     private static final String A_COLOR = "a_Color";
-    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int POSITION_COMPONENT_COUNT = 4;
     private static final int COLOR_COMPONENT_COUNT = 3;
     private static final int BYTES_PER_FLOAT = 4;
     private static final int STRIDE =
@@ -49,32 +52,34 @@ public class AirHockeyRenderer implements Renderer {
 
     private final FloatBuffer vertexData;
     private final Context context;
+    private final float[] projectionMatrix = new float[16];
 
     private int program;
     private int aPositionLocation;
     private int aColorLocation;
+    private int uMatrixLocation;
 
     public AirHockeyRenderer(Context context) {
         this.context = context;
         float[] tableVerticesWithTriangles = {
 
-                // Order of coordinates: X, Y, R, G, B
+                // Order of coordinates: X, Y, Z, W, R, G, B
 
                 // Triangle Fan
-                   0f,    0f,   1f,   1f,   1f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0f,    0f, 0f, 1.5f,   1f,   1f,   1f,
+                -0.5f, -0.8f, 0f,   1f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.8f, 0f,   1f, 0.7f, 0.7f, 0.7f,
+                0.5f,  0.8f, 0f,   2f, 0.7f, 0.7f, 0.7f,
+                -0.5f,  0.8f, 0f,   2f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0f,   1f, 0.7f, 0.7f, 0.7f,
 
                 // Line 1
-                -0.5f, 0f, 1f, 0f, 0f,
-                0.5f, 0f, 1f, 0f, 0f,
+                -0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
+                0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
 
                 // Mallets
-                0f, -0.25f, 0f, 0f, 1f,
-                0f,  0.25f, 1f, 0f, 0f
+                0f, -0.4f, 0f, 1.25f, 0f, 0f, 1f,
+                0f,  0.4f, 0f, 1.75f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer
@@ -108,6 +113,8 @@ public class AirHockeyRenderer implements Renderer {
         }
         glUseProgram(program);
 
+        uMatrixLocation = glGetUniformLocation(program, U_MATRIX);
+
         aPositionLocation = glGetAttribLocation(program, A_POSITION);
         aColorLocation = glGetAttribLocation(program, A_COLOR);
 
@@ -131,12 +138,24 @@ public class AirHockeyRenderer implements Renderer {
     public void onSurfaceChanged(GL10 glUnused, int width, int height) {
         // Set the OpenGL viewport to fill the entire surface.
         glViewport(0, 0, width, height);
+        final float aspectRatio = width > height ?
+                (float) width / (float) height :
+                (float) height / (float) width;
+        if (width > height) {
+            // Landscape
+            orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
+        } else {
+            // Portrait or square
+            orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
+        }
     }
     @Override
     public void onDrawFrame(GL10 glUnused) {
 
             // Clear the rendering surface.
             glClear(GL_COLOR_BUFFER_BIT);
+            // Assign the matrix
+            glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
 
             // Draw the table.
             glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
